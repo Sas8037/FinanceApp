4f74826be3e79377de312c489001425287324445
Saul Saavedra <Sas8037@icloud.com>
2025-01-16 15:29:46 -0600

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Saul Saavedra on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
