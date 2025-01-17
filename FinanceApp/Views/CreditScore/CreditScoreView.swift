9625b8e569169a6d58d2663a096779b3a5b811d5
Saul Saavedra <Sas8037@icloud.com>
2025-01-16 22:45:14 -0600

    Added base code for "CreditScoreView.swift"

--- FinanceApp/Views/CreditScore/CreditScoreView.swift
+++ FinanceApp/Views/CreditScore/CreditScoreView.swift
@@ -5,4 +5,17 @@
 //  Created by Saul Saavedra on 1/16/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct CreditScoreView: View {
+    var body: some View {
+        VStack {
+            Text("No data available")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    CreditScoreView()
+}
