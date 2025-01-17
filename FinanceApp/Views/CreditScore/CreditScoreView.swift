8c95ca54fd3d0395764d57512f5c5b0670f2c46b
Saul Saavedra <Sas8037@icloud.com>
2025-01-16 22:07:26 -0600

    Added the file "CreditScoreView.swift"

---
+++ FinanceApp/Views/CreditScore/CreditScoreView.swift
@@ -0,0 +1,21 @@
+//
+//  CreditScoreView.swift
+//  FinanceApp
+//
+//  Created by Saul Saavedra on 1/16/25.
+//
+
+import SwiftUI
+
+struct CreditScoreView: View {
+    var body: some View {
+        VStack {
+            Text("No data available")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    CreditScoreView()
+}
